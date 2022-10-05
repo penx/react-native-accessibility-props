8c83766a8311e4d9faea2521738328c36d98a697
Alasdair McLeay <alasdair.mcleay@me.com>
2022-10-05 09:24:40 +0100

    Initial functionality

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ Get cross platform accessibily props for react-native and react-native-web.
 
 - <https://github.com/necolas/react-native-web/issues/2403#issuecomment-1267502015>
 - <https://github.com/necolas/react-native-web/discussions/2372>
+
+## Usage
+
+```tsx
+import { Pressable } from "react-native";
+import { getAccessibilityStateProps } from "react-native-accessibility-props";
+
+function MyComponent() {
+  return (
+    <Pressable
+      accessibilityRole="radio"
+      accessibilityLabel="label"
+      {...getAccessibilityStateProps({ checked: true })}
+    >
+      Example
+    </Pressable>
+  );
+}
+```
--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
-console.log('initial template')
+import { Platform } from "react-native";
 
-export {}
+export const getAccessibilityStateProps = (accessibilityState: {
+  disabled?: boolean;
+  selected?: boolean;
+  checked?: boolean | "mixed";
+  busy?: boolean;
+  expanded?: boolean;
+}) =>
+  Platform.select({
+    web: {
+      accessibilityDisabled: accessibilityState.disabled,
+      accessibilitySelected: accessibilityState.selected,
+      accessibilityChecked: accessibilityState.checked,
+      accessibilityBusy: accessibilityState.busy,
+      accessibilityExpanded: accessibilityState.expanded,
+    },
+    default: { accessibilityState },
+  });
